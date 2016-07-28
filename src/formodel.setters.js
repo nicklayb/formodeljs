2fa08f0878a88fa94658a80ef304f80978b543e6
Nicolas Boisvert <nboisvert@armrecouvrement.com>
2016-07-28 09:07:05 -0400

    Allow checkbox and radio button to be checked

--- src/formodel.setters.js
+++ src/formodel.setters.js
@@ -3,17 +3,20 @@ Formodel.prototype.setRecordId = function (id) {
     this.recordId = (id !== undefined && id !== null && id > -1 && !isNaN(id)) ? id : -1;
 };
 
-
 Formodel.prototype.fillInput = function (tag, name, value) {
     switch (tag.toLowerCase()) {
         case 'checkbox':
         case 'radio':
+            var selector = 'input[type="' + tag + '"][name="' + name + '"]';
+            $(selector).prop('checked', value).change();
             break;
         default:
+            value = (typeof value == 'string') ? value.trim() : value;
             var selector = tag + '[name="' + name + '"]';
-            $(selector).val(value.trim());
+            $(selector).val(value);
     }
 };
+
 Formodel.prototype.fillWith = function (record) {
     var attributes = this.getAttributes();
     for(var key in attributes){
